8a2e0c16c48ad7078b01c8f98359ed4113c00a71
Tayami <tayamirai6@gmail.com>
2024-10-08 06:10:35 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Tayami Rai on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
